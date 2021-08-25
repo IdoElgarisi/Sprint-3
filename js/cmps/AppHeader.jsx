5831383ef35948372452de0ed59ddc366ccbe033
IdoElgarisi <ido12el@gmail.com>
2021-08-25 10:30:50 +0300

    "work start commit"

--- js/cmps/AppHeader.jsx
+++ js/cmps/AppHeader.jsx
@@ -3,11 +3,13 @@ class _AppHeader extends React.Component {
     render() {
         return (
             <section className="app-header">
-                <h1 ><NavLink exact to="/">Book Store</NavLink></h1>
+                <h1 className="logo" ><NavLink exact to="/">AppSus</NavLink></h1>
                 <ul className="main-nav-bar">
-                    <li> <NavLink exact to="/">Home</NavLink></li>
+                    <li> <NavLink exact to="/"><i className="fa fa-home"></i></NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/book">Our Books</NavLink></li>
+                    <li><NavLink to="/keepApp"><i className="fa fa-sticky-note"></i></NavLink></li>
+                    <li><NavLink to="/emailApp"><i className="fa fa-envelope-open"></i></NavLink></li>
                 </ul>
 
             </section>
--- js/pages/Home.jsx
+++ js/pages/Home.jsx
@@ -2,10 +2,10 @@ const { NavLink } = ReactRouterDOM
 export function Home() {
     return (
     <main className="main-layout">
-        <h1>Welcome To My Online Book Store</h1>
-        <button><NavLink to="/emailApp">Go To Email</NavLink></button>
+        <h1>Welcome To Sprint 3 Home</h1>
+        {/* <button><NavLink to="/emailApp">Go To Email</NavLink></button>
         <button><NavLink to="/book">Go To Store</NavLink></button>
-        <button><NavLink to="/keepApp">Go To Keep</NavLink></button>
+        <button><NavLink to="/keepApp">Go To Keep</NavLink></button> */}
     </main>
 )
 }
