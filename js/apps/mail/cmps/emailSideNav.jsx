b026e5fe1911ce499424ddc281eb58f1897782b4
IdoElgarisi <ido12el@gmail.com>
2021-08-25 19:12:12 +0300

    ido mail-nav sorting

---
+++ js/apps/mail/cmps/emailSideNav.jsx
@@ -0,0 +1,20 @@
+const { NavLink, withRouter } = ReactRouterDOM
+class _EmailNav extends React.Component {
+    render() {
+        return (
+            <section className="mail-nav">
+                <button className="new-mail-btn flex"><div className="add-img"></div> <p>New Mail</p></button>
+                <ul className="mail-nav-bar">
+                    <li><i className="fa fa-inbox"></i><span>Inbox</span> </li>
+                    <li><i className="fa fa-star"></i><span>Stared</span> </li>
+                    <li><i className="fa fa-paper-plane"></i><span>Sent</span> </li>
+                    <li><i className="fa fa-sticky-note"></i> <span>Drafts</span></li>
+                    <li><i className="fa fa-trash"></i> <span>Rcycle Bin</span></li>
+                </ul>
+
+            </section>
+        )
+    }
+}
+
+export const EmailNav = withRouter(_EmailNav);
